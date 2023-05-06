901ccc34b4afbf780dc6da76fd35346e4d433050
bemejia <b.mejial@uniandes.edu.co>
2023-05-06 10:50:55 -0500

    IMPL_HU07_BEMEJIA: Crear album imagen y datepicker

--- app/src/main/java/com/mobiles/vinilosapp/ui/albums/AlbumCreateFragment.kt
+++ app/src/main/java/com/mobiles/vinilosapp/ui/albums/AlbumCreateFragment.kt
@@ -1,15 +1,20 @@
 package com.mobiles.vinilosapp.ui.albums
 
 import android.R
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.DatePicker
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.textfield.TextInputEditText
@@ -17,16 +22,38 @@ import com.mobiles.vinilosapp.databinding.AlbumCreateFragmentBinding
 import com.mobiles.vinilosapp.models.Album
 import com.mobiles.vinilosapp.viewmodels.AlbumViewModel
 
+
 class AlbumCreateFragment:  Fragment() {
 
     private var _binding: AlbumCreateFragmentBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: AlbumViewModel
+    private lateinit var btnImage: ImageView
+    private lateinit var pickMediaLauncher: ActivityResultLauncher<PickVisualMediaRequest>
+    private lateinit var uri : Uri
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pickMediaLauncher = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+            if(it != null){
+                uri = it
+                btnImage.setImageURI(uri)
+            }else{
+                //No image
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = AlbumCreateFragmentBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        btnImage = _binding?.albumCreateImage!!
+        btnImage.setOnClickListener{
+
+            pickMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
         return view
     }
 
@@ -42,6 +69,17 @@ class AlbumCreateFragment:  Fragment() {
 
         _binding?.albumCreateButton?.setOnClickListener(createAlbumListener())
 
+        _binding?.etDate?.setOnClickListener { showDatePickerDialog(activity) }
+
+    }
+
+    private fun showDatePickerDialog(activity : FragmentActivity) {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(activity.supportFragmentManager,  "datePicker")
+    }
+
+    fun onDateSelected(day:Int, month:Int, year:Int){
+        _binding?.etDate?.setText("Has seleccionado el dia $day del mes $month de $year")
     }
 
     private fun setDropDowns() {
@@ -61,8 +99,6 @@ class AlbumCreateFragment:  Fragment() {
         return View.OnClickListener { view ->
 
             val nameTxt : TextInputEditText? = _binding?.txtAlbumName
-            val imageTxt : TextInputEditText? = _binding?.txtAlbumImage
-            val datePkr : DatePicker? = _binding?.txtAlbumDate
             val descTxt : TextInputEditText? = _binding?.txtAlbumDesc
             val discTxt : AutoCompleteTextView? = _binding?.txtAlbumDisc
             val genreTxt : AutoCompleteTextView? = _binding?.txtAlbumGenre
@@ -70,7 +106,7 @@ class AlbumCreateFragment:  Fragment() {
             val album = Album(
                 albumId =  null,
                 name = nameTxt?.text.toString(),
-                cover = imageTxt?.text.toString(),
+                cover = uri.toString(),
                 releaseDate = "1984-08-01T00:00:00-05:00",
                 description = descTxt?.text.toString(),
                 genre = genreTxt?.text.toString(),
